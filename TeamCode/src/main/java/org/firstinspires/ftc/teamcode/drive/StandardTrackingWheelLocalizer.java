dbcaf278f028ed05ebb7f34bc6c47f27a1d5bdc3
LordVanra <arnav.omkar@gmail.com>
2024-07-16 14:25:43 -0400

    Robot Commited Suicide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 7.75; // in; distance between the left and right wheels 4.125
+    public static double LATERAL_DISTANCE = 7.8537; // in; distance between the left and right wheels 4.125
     public static double FORWARD_OFFSET = -4.6; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
